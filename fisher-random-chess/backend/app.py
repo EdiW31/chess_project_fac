4e8e61e17f59f8fbda0afde723bd2938e2f6463a
Lavu Cristian <cristianioan.lavu@ulbsibiu.ro>
2025-05-11 22:20:39 +0300

    Adaugat bd pentru AI si jumate din  joc in retea

--- fisher-random-chess/backend/app.py
+++ fisher-random-chess/backend/app.py
@@ -421,6 +421,6 @@ def reset_learning_data():
     }
     save_learning_data(learning_data)
     return jsonify({"status": "success", "message": "Learning data reset."})
-
+#moga
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
